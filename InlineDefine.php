2cb313876bcf05c35b5bfec87f801ae0925487c1
Jason McE <git@brightshinyobject.com>
2013-10-27 21:30:56 +1100

    [tags/1.0.0] Initial create.

---
+++ InlineDefine.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * simple-inline-define.php
+ *
+ * Most basic version of inline-text replacement class.  Later I will expand
+ * this into a more useful set of classes and features.
+ *
+ */
+
+
+
+/**
+ * InlineDefine class
+ *
+ * An object which will replace terms in a string with their definitions
+ * as supplied, which may include dom wrapped strings with various html
+ * attributes to be used separately.
+ */
+class InlineDefine
+{
+    /**
+     * Returns text with all terms replaced by their decorated forms.
+     *
+     * @param string $text  Text to be updated.
+     * @param array  $terms Array of term => wrapped text. The wrapped text
+     *     can include anything the user wants, including html or javascript.
+     *
+     * @return string The original text, after text replacement.
+     */
+    public function getDecoratedText($text = null, $terms = null)
+    {
+        foreach ($terms as $term => $definition) {
+            $text = str_replace($term, $definition, $text);
+        }
+        return $text;
+    }
+}
+
---
+++ tests/InlineDefineTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * This test class provides basic error checking for the InlineDefine object.
+ */
+
+require 'InlineDefine.php';
+
+class InlineDefineTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Confirm the constructor returns an object.
+     *
+     * @return object Returns nothing.
+     */
+    public function testEmptyConstructor()
+    {
+        $inline_text = new InlineDefine();
+        $this->assertNotEmpty($inline_text);
+    }
+
+    /**
+     * Confirm the getDecoratedText method with a simple test.
+     * This should later be replaced with a large textfile and more dynamic
+     * set of term => definition pairs, including words within another term's
+     * decorated text.
+     *
+     * @return bool True is test passes.
+     */
+    public function testGetDecoratedTextSimpleText()
+    {
+        $test_text = "An apple a day beats two pear.";
+        $test_terms = array(
+          'apple' =>
+                '<a href="http://apple.com" title="fiber-tastic fruit">apple</a>',
+          'pear' => '<a href="http://pear.com" title="juicy fruit">pear</a>',
+        );
+
+        $inline_text = new InlineDefine();
+        $result_text = $inline_text->getDecoratedText($test_text, $test_terms);
+
+        $this->assertEquals(
+            $result_text,
+            'An <a href="http://apple.com" title="fiber-tastic fruit">apple</a> ' .
+            'a day beats two <a href="http://pear.com" title="juicy fruit">pear</a>.'
+        );
+    }
+
+    /**
+    * Try the getDecoratedText method as a static object method.
+    *
+    * @return bool true if result matches expected.
+    */
+    public function testGetDecoratedTextAsStaticMethod()
+    {
+        $test_text = "An apple a day beats two pear.";
+        $test_terms = array(
+          'apple' =>
+              '<a href="http://apple.com" title="fiber-tastic fruit">apple</a>',
+          'pear' => '<a href="http://pear.com" title="juicy fruit">pear</a>',
+        );
+
+        $result_text = InlineDefine::getDecoratedText($test_text, $test_terms);
+
+        $this->assertEquals(
+            $result_text,
+            'An <a href="http://apple.com" title="fiber-tastic fruit">apple</a> a' .
+            ' day beats two <a href="http://pear.com" title="juicy fruit">pear</a>.'
+        );
+    }
+
+}
